93b29c6b428b0864019c71d96e8e94d59943f6be
Javier Fernández <leqjl93@hotmail.com>
2024-03-07 18:45:02 -0600

    x

--- system/classes/Infinity/CatalogCurrency.php
+++ system/classes/Infinity/CatalogCurrency.php
@@ -3,80 +3,47 @@
 namespace Infinity;
 
 use HCStudio\Orm;
+use JFStudio\Constants;
 
 class CatalogCurrency extends Orm {
 	protected $tblName = 'catalog_currency';
-	public static $USD = 1;
-	public static $MXN = 2;
-	public static $COP = 3;
-	public static $BTC = 4;
-	public static $PYT = 5;
+
+	const USD = 8;
 	public function __construct() {
 		parent::__construct();
 	}
 
-	public function getCatalogCurrencyName($currency = null) {
-		if($currency == self::$USD) {
-			return "Dólar";
-		} else if($currency == self::$MXN) {
-			return "Pesos mexicanos";
-		} else if($currency == self::$COP) {
-			return "Pesos colombianos";
-		} else if($currency == self::$USD) {
-			return "Bitcoins";
-		} else if($currency == self::$PYT) {
-			return "PayToken";
-		}
-	}
-
-	public function getCatalogCurrencySingleName($currency = null) {
-		if($currency == self::$USD) {
-			return "USD";
-		} else if($currency == self::$MXN) {
-			return "MXN";
-		} else if($currency == self::$USD) {
-			return "COP";
-		} else if($currency == self::$USD) {
-			return "BTC";
-		}
-	}
-	public function getCurrencyByCountryId($country_id = null) {
-		if(isset($country_id) === true)
-		{
-			$sql = "SELECT 	
-						{$this->tblName}.currency
-					FROM 
-						{$this->tblName}
-					WHERE
-						{$this->tblName}.country_id = '{$country_id}'
-					";
-
-			return $this->connection()->field($sql);
-		}
-		
-		return false;
-	}
-
-	public function getCatalogCurrencyByCountryId($country_id = null) 
+	public function getAll()
 	{
-		$country_id = $country_id == false ? 159 : $country_id; 
-		if(isset($country_id) === true)
-		{
-			$sql = "SELECT 	
-						{$this->tblName}.{$this->tblName}_id
-					FROM 
-						{$this->tblName}
-					WHERE
-						{$this->tblName}.country_id = '{$country_id}'
-					";
-
-			return $this->connection()->field($sql);
-		}
+		$sql = "SELECT 
+					{$this->tblName}.{$this->tblName}_id,
+					{$this->tblName}.currency,
+					{$this->tblName}.description,
+					{$this->tblName}.code
+				FROM 
+					{$this->tblName}
+				WHERE 
+					{$this->tblName}.status = '1'
+				";
 		
-		return false;
+		return $this->connection()->rows($sql);
 	}
-
-
+	
+	public function getCurrency(int $catalog_currency_id = null)
+	{
+		$sql = "SELECT 
+					{$this->tblName}.currency
+				FROM 
+					{$this->tblName}
+				WHERE 
+					{$this->tblName}.catalog_currency_id = '{$catalog_currency_id}'
+				AND  
+					{$this->tblName}.status = '1'
+				";
+		
+		return $this->connection()->field($sql);
+	}
+	
 	public function getFullCurrency(int $catalog_currency_id = null)
 	{
 		$sql = "SELECT 
@@ -92,5 +59,27 @@ class CatalogCurrency extends Orm {
 				";
 		
 		return $this->connection()->row($sql);
+	}
+
+	public function getIn(string $catalog_currency_ids = null)
+	{
+		if(isset($catalog_currency_ids) === true)
+		{
+			$sql = "SELECT 
+						{$this->tblName}.{$this->tblName}_id,
+						{$this->tblName}.currency,
+						{$this->tblName}.description
+					FROM 
+						{$this->tblName}
+					WHERE 
+						{$this->tblName}.catalog_currency_id IN ({$catalog_currency_ids})
+					AND  
+						{$this->tblName}.status = '".Constants::AVIABLE."'
+					";
+			
+			return $this->connection()->rows($sql);
+		}
+
+		return false;
 	}
 }
